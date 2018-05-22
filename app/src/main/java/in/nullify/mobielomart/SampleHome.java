d39d8f6e869657b2b51d835decf635a1cb30cd39
Aswin Divakar <aswindivakar100@gmail.com>
2018-05-23 01:28:07 +0530

    Google Signin & Normal Signin working;

--- app/src/main/java/in/nullify/mobielomart/SampleHome.java
+++ app/src/main/java/in/nullify/mobielomart/SampleHome.java
@@ -1,16 +1,13 @@
 package in.nullify.mobielomart;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SampleHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(this.getApplicationContext(),SignInActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/in/nullify/mobielomart/SignInActivity.java
+++ app/src/main/java/in/nullify/mobielomart/SignInActivity.java
@@ -1,15 +1,27 @@
 package in.nullify.mobielomart;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 import org.json.JSONObject;
 
@@ -29,18 +41,46 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by Aswin on 18-05-2018.
  */
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
     private String name;
     private ArrayAdapter adapter;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private GoogleApiClient mGoogleApiClient;
+    private ProgressDialog mProgressDialog;
+
+    private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        final EditText SignupEmail = (EditText) findViewById(R.id.signinEmail);
-        final EditText SignupPassword = (EditText) findViewById(R.id.signinPassword);
-        Button SignupButton = (Button) findViewById(R.id.signinButton);
+        btnSignIn = findViewById(R.id.btn_signin_google);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().requestId().requestProfile()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Customizing G+ button
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        signIn();
+            }
+        });
+
+
+        final EditText SignupEmail = (EditText) findViewById(R.id.et_signin_email);
+        final EditText SignupPassword = (EditText) findViewById(R.id.et_signin_pwd);
+        Button SignupButton = (Button) findViewById(R.id.btn_signin);
 
         SignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +94,111 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
     }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e(TAG, "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            String personPhotoUrl = acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Log.e(TAG, "Name: " + personName + ", email: " + email
+                    + ", Image: " + personPhotoUrl);
+
+            //new SignUp().execute(personName, email, "password", null, "Result");
+            Log.e("Hoi ",new PostMan().execute("https://www.nullify.in/mobielo_mart/php/signup" +
+                    ".php","name",personName,"email", email,"pass", "password", null, "Result").toString());
+
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            Toast.makeText(this,"Done Already",Toast.LENGTH_LONG).show();
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+
+
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    public void SignUpResult(String string){
+        Log.e("Tag",string);
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+        hideProgressDialog();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    private void updateUI(Object o) {
+        hideProgressDialog();
+        if(o.equals(true)){
+            Intent intent=new Intent(this.getApplicationContext(),SampleHome.class);
+            startActivity(intent);
+        }
+    }
+
 
     public class Signup extends AsyncTask<String, Void, String> {
 
